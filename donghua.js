5e697e42a5d7d656fd4001fed28927b964434210
Sam Wierema <mail@samwierema.nl>
2012-03-24 23:46:48 +0100

    Prepare for loading screen
    Add a spinner and wait for all the images to have triggered the onload event before displaying them. Also optimize the javascript.

--- donghua.js
+++ donghua.js
@@ -1,16 +1,37 @@
 var sp = getSpotifyApi(1);
 var models = sp.require('sp://import/scripts/api/models');
+
 var player = models.player;
+player.repeat = true;
 
 exports.init = init;
 
-var list = [],
-    playlist = [];
+var gifs = [],
+    playlist = [],
+    loaded = 0;
 
 function init() {
-	// request 
-	$('#header').html('loading...');
-	getList();
+  
+  var opts = {
+    lines: 11, // The number of lines to draw
+    length: 23, // The length of each line
+    width: 10, // The line thickness
+    radius: 31, // The radius of the inner circle
+    rotate: 10, // The rotation offset
+    color: '#000', // #rgb or #rrggbb
+    speed: 1, // Rounds per second
+    trail: 60, // Afterglow percentage
+    shadow: false, // Whether to render a shadow
+    hwaccel: false, // Whether to use hardware acceleration
+    className: 'spinner', // The CSS class to assign to the spinner
+    zIndex: 2e9, // The z-index (defaults to 2000000000)
+    top: 'auto', // Top position relative to parent in px
+    left: 'auto' // Left position relative to parent in px
+  };
+  var target = document.getElementById('content');
+  var spinner = new Spinner(opts).spin(target);
+  
+  getList();
 	
 	sp.core.addEventListener("argumentsChanged", argumentsChanged);
 	
@@ -36,51 +57,58 @@ function argumentsChanged() {
 }
 
 function showGif(position) {
-  var o = list[position];
+  var gif = gifs[position];
+  console.log(gif);
   
-  var img = $('<img class="full">').attr('src', o['gif']);
+  var img = $('<img class="full">').attr('src', gif);
   $('#content').html(img);
 }
 
 function goto(position) {
   showGif(position);
   
-  var o = list[position];
   // play!!
-  player.play(o.track_id, playlist); 
+  player.play(playlist.tracks[position].uri, playlist); 
 }
 
 function getList() {
 	$.ajax({
     url: 'http://lab.wiewer.nl/musichackday/2012/amsterdam/gifs.json',
     dataType: 'json',
-    success: function(data) {
-      $('#header').html('bang...');
-      list = data;
-      
+    success: function(data) {      
       // create playlist!!
       playlist = new models.Playlist();
-      var i;
-      for(i = 0; i < list.length; i++) {
-        playlist.add(list[i].track_id);
+      for(var i = 0; i < data.length; i++) {
+        models.Track.fromURI(data[i].track_id, function(track) {
+          if(track.playable) {
+            playlist.add(track);
+            gifs.push(data[i].gif);
+          }
+        })
       }
-      
-      player.repeat = true;
-      
       showList();
   	}
   });
 }
 
 function showList() {
-  $('#content').html('<div id="list" />');
-  var i;
-  for(i in list) {
-    var o = list[i];
-    var img = $('<img/>').attr('src', o['gif']);
+  $('#content').append($('<div id="list" />').css('display', 'none'));
+  for(var i in gifs) {
+    var gif = gifs[i];
+    var img = $('<img/>').attr('src', gif).on('load', function() {
+      incrLoaded();
+    });
     var a = $('<a/>').attr('href', 'spotify:app:donghua:' + i)
     a.append(img);
     $('#list').append(a);
+  }  
+}
+
+function incrLoaded() {
+  loaded++;
+  if(loaded == playlist.tracks.length) {
+    // hide spinner
+    $('.spinner').hide();
+    $('#list').show();
   }
-  
 }
