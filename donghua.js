acd3147ce40e4b610c86edae9e3cfcee4311f2c4
Sam Wierema <mail@samwierema.nl>
2012-03-25 11:49:41 +0200

    Super nice loading screen

--- donghua.js
+++ donghua.js
@@ -11,26 +11,6 @@ var gifs = [],
     loaded = 0;
 
 function init() {
-  
-  var opts = {
-    lines: 11, // The number of lines to draw
-    length: 23, // The length of each line
-    width: 10, // The line thickness
-    radius: 31, // The radius of the inner circle
-    rotate: 10, // The rotation offset
-    color: '#000', // #rgb or #rrggbb
-    speed: 1, // Rounds per second
-    trail: 60, // Afterglow percentage
-    shadow: false, // Whether to render a shadow
-    hwaccel: false, // Whether to use hardware acceleration
-    className: 'spinner', // The CSS class to assign to the spinner
-    zIndex: 2e9, // The z-index (defaults to 2000000000)
-    top: 'auto', // Top position relative to parent in px
-    left: 'auto' // Left position relative to parent in px
-  };
-  var target = document.getElementById('content');
-  var spinner = new Spinner(opts).spin(target);
-  
   getList();
 	
 	sp.core.addEventListener("argumentsChanged", argumentsChanged);
@@ -108,7 +88,7 @@ function incrLoaded() {
   loaded++;
   if(loaded == playlist.tracks.length) {
     // hide spinner
-    $('.spinner').hide();
+    $('#loading').hide();
     $('#list').show();
   }
 }
