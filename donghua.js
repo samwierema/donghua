b73029024964d8c0da58c633f78d9997d7be5c5e
Sam Wierema <mail@samwierema.nl>
2012-03-25 13:53:32 +0200

    The almost final version, I think

--- donghua.js
+++ donghua.js
@@ -44,11 +44,52 @@ function argumentsChanged() {
 }
 
 function showGif(position) {
-  var gif = gifs[position];
-  console.log(gif);
-  
-  var img = $('<img class="full">').attr('src', gif);
-  $('#content').html(img);
+  if($('#fullscreen').is(":hidden")) {
+    var img = $('<img />').attr({
+      'src': gifs[position]
+    });
+    $('#fullscreen').append(img);
+    $('#fullscreen a').click(function() {
+      player.playing = false;
+      showMainScreen();
+    })
+    hideLoadingScreen();
+    hideMainScreen();
+    $('#fullscreen').show();
+  }
+}
+
+function hideGif() {
+  $('#fullscreen img').remove();
+  $('#fullscreen').hide();
+}
+
+function showMainScreen() {
+  hideLoadingScreen();
+  hideGif();
+  $('body').css({'margin-top': '5%'});
+  $('#list').show();
+  $('#footer').show();
+}
+
+function hideMainScreen() {
+  $('#list').hide();
+  $('#footer').hide();
+}
+
+function showLoadingScreen() {
+  hideMainScreen();
+  hideGif();
+  spinner.spin(target);
+  if($(window).height() > 290) {
+    $('body').css({'margin-top': (($(window).height() - 290) / 2 + 'px')});
+  }
+  $('#loading').show();
+}
+
+function hideLoadingScreen() {
+  spinner.stop();
+  $('#loading').hide();
 }
 
 function goto(position) {
@@ -79,10 +120,12 @@ function getList() {
 }
 
 function showList() {
-  $('#content').append($('<div id="list" />').css('display', 'none'));
   for(var i in gifs) {
     var gif = gifs[i];
-    var img = $('<img/>').attr('src', gif).on('load', function() {
+    var img = $('<img/>').attr('src', gif).css({
+      'width': ($(document).width() / 4),
+      'height': 'auto'
+    }).on('load', function() {
       incrLoaded();
     });
     var a = $('<a/>').attr('href', 'spotify:app:donghua:' + i)
@@ -94,8 +137,6 @@ function showList() {
 function incrLoaded() {
   loaded++;
   if(loaded == playlist.tracks.length) {
-    // hide spinner
-    $('#loading').hide();
-    $('#list').show();
+    showMainScreen();
   }
 }
